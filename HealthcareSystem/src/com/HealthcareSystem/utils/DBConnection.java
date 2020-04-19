9d19a5a071a049e523afef66f1227fef78320c75
NimshiAshera <38455210+NimshiAshera@users.noreply.github.com>
2020-04-19 10:34:13 +0530

    Update DBConnection.java

--- HealthcareSystem/src/com/HealthcareSystem/utils/DBConnection.java
+++ HealthcareSystem/src/com/HealthcareSystem/utils/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
 		try {
 
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/paf?useSSL=false", "root", "root");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/paf?useSSL=false", "root", "");
 
 		} 
 		catch (Exception e)
